cfa56c6341c9cec4048a1f4c6df89263e8296132
Cayo <globalsys@Globalsyss-MacBook-Pro.local>
2019-12-10 15:26:40 -0300

    Fix

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import connectToState from "./connectToState"
 import CCF from "./CreditCardInput"
 import LiteCCF from "./LiteCreditCardInput"
 import CV from "./CardView"
-import AcceptedCards from './AcceptedCards'
+import ACards from './AcceptedCards'
 
 export const CreditCardInput = connectToState(CCF)
 export const LiteCreditCardInput = connectToState(LiteCCF)
 export const CardView = CV
 
-export const AcceptedCards
+export const AcceptedCards = ACards
