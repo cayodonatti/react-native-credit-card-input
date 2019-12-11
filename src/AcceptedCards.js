89185db30dc469470333171724528a45b99e1fbc
Cayo <globalsys@Globalsyss-MacBook-Pro.local>
2019-12-11 08:41:40 -0300

    adding new brands

--- src/AcceptedCards.js
+++ src/AcceptedCards.js
@@ -5,6 +5,9 @@ const AcceptedCards = {
   discover: require("./icons/stp_card_discover.png"),
   visa: require("./icons/stp_card_visa.png"),
   elo: require("./icons/stp_card_elo.png"),
+  hipercard: require("./icons/stp_card_hipercard.png"),
+  hiper: require("./icons/stp_card_hiper.png"),
+  maestro: require("./icons/stp_card_maestro.png"),
 };
 
 export default AcceptedCards;
